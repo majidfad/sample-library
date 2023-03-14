226b6098a8bec822120857d5b75de84b0fd19e95
majidfadavi <majidfadavi1375@gmail.com>
2023-03-14 11:44:13 +0330

    files added :)

---
+++ src/test/java/com/library/sample/samplelibrary/SampleLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.sample.samplelibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
